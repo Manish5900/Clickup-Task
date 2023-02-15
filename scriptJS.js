5d23e63f23a62ca23f33d60284418fd286fa52a6
Manish Singh <manish.s@brilworks.com>
2023-02-15 17:11:39 +0530

    Apply JS functionality updated

--- scriptJS.js
+++ scriptJS.js
@@ -1,23 +1,36 @@
 // script for popup
 const addRecordPop = () => {
   let data = document.querySelector("#popup");
-
   if (data.classList.contains("disblank")) {
     data.classList.remove("disblank");
   } else {
     data.classList.add("disblank");
   }
-  tableRefresh();
+};
+
+const dataList = () => {
+  document.getElementById("insert-page").innerHTML = "";
+  let noOfPage = localStorage.getItem("TABLE");
+  noOfPage = JSON.parse(noOfPage);
+
+  for (let i = 0; i < noOfPage.length / 4; i++) {
+    console.log(noOfPage.length / 4);
+
+    document.getElementById("insert-page").innerHTML =
+      document.getElementById("insert-page").innerHTML +
+      ` <li class="" id="select${i}"> <a href="#" onclick="changePage(${i})">${
+        i + 1
+      }</a></li>`;
+  }
 };
 
 const tableRefresh = () => {
   document.getElementById("insert_data").innerHTML = "";
   if (localStorage.getItem("TABLE")) {
-    let tabel_data = localStorage.getItem("TABLE");
+    let table_data = localStorage.getItem("TABLE");
     //  converting  readable data from json
-
-    tabel_data = JSON.parse(tabel_data);
-    tabel_data.forEach((data) => {
+    table_data = JSON.parse(table_data);
+    table_data.forEach((data, index) => {
       let ID = String(data.ID);
 
       document.getElementById("insert_data").innerHTML += `
@@ -28,9 +41,15 @@ const tableRefresh = () => {
             <td>${data.address}</td>
             <td>
               <div>
-                <button Onclick = "view(${data.srno})"><i class="fa-regular fa-eye"></i>View</button>
-                <button Onclick= "open_delete_popup(${data.srno})"><i class="fa-solid fa-trash"></i>Delete</button>
-                <button Onclick= "edit_data_popup(${data.srno},${ID})"><i class="fa-solid fa-pen-to-square"></i>Edit</button>
+                <button Onclick = "view(${
+                  data.ID
+                })"><i class="fa-regular fa-eye"></i>View</button>
+                <button Onclick= "open_delete_popup(${
+                  data.ID
+                })"><i class="fa-solid fa-trash"></i>Delete</button>
+                <button Onclick= "editPopup(${
+                  index + 1
+                },${ID})"><i class="fa-solid fa-pen-to-square"></i>Edit</button>
               </div>
             </td>
           </tr>`;
@@ -38,9 +57,53 @@ const tableRefresh = () => {
   }
 };
 
-const close_box = () => {
-  const data = document.getElementById("view_data");
+// For Edit section
+
+const close_box_edit = () => {
+  const data = document.getElementById("edit_data_popup");
   data.classList.add("disblank");
+};
+
+const editPopup = (srno, ID) => {
+  const data = document.getElementById("edit_data_popup");
+  data.classList.remove("disblank");
+  let populateData = localStorage.getItem("TABLE");
+  populateData = JSON.parse(populateData);
+  let currentData = populateData.filter((data) => data.ID == ID);
+  document.getElementById("E_Location_name").value =
+    currentData[0].LocationName;
+  document.getElementById("E_textbox").value = currentData[0].address;
+  const callFunction = document.getElementById("Ebutton-popup");
+  callFunction.innerHTML = `<button onclick="editData(${srno},${ID})" type="submit" class="no">Save</button> `;
+};
+
+const editData = (srno, ID) => {
+  let data = localStorage.getItem("TABLE");
+  data = JSON.parse(data);
+  let curData = data.filter((data) => data.ID != ID);
+  let newLocation = document.getElementById("E_Location_name").value;
+  let newAdress = document.getElementById("E_textbox").value;
+  var updatedData = {
+    srno: srno,
+    ID: ID,
+    LocationName: newLocation,
+    address: newAdress,
+  };
+  curData.push(updatedData);
+  localStorage.removeItem("TABLE");
+  localStorage.setItem("TABLE", JSON.stringify(curData));
+};
+
+// For delete section
+// this will insiate popup of delete  and then connect it with  delete function
+const open_delete_popup = (ID) => {
+  const data = document.getElementById("Delete_view");
+  data.classList.remove("disblank");
+
+  const call_fun = document.getElementById("button-popup");
+  console.log(data);
+  call_fun.innerHTML = `<button onclick="Delete(${ID})" type="button" class="yes"> YES </button> 
+                        <button onclick="close_box_del()" type="submit" class="no">NO</button>`;
 };
 
 const close_box_del = () => {
@@ -48,128 +111,76 @@ const close_box_del = () => {
   data.classList.add("disblank");
 };
 
-const close_box_edit = () => {
-  const data = document.getElementById("edit_data_popup");
-  data.classList.add("disblank");
-};
+const Delete = (ID) => {
+  try {
+    let table;
+    table = localStorage.getItem("TABLE");
+    table = JSON.parse(table);
 
-const edit_data_popup = (srno, ID) => {
-  const data = document.getElementById("edit_data_popup");
-  data.classList.remove("disblank");
-  const call_fun = document.getElementById("Ebutton-popup");
-  console.log(call_fun);
-  call_fun.innerHTML = `<button onclick="edit_data(${srno},${ID})" type="submit" class="no"> Save </button> `;
-};
-
-const edit_data = (srno, ID) => {
-  let data = localStorage.getItem("TABLE");
-  data = JSON.parse(data);
-  let cur_data = data.filter((data) => data.srno != srno);
-  let new_loc = document.getElementById("E_Location_name").value;
-  let new_address = document.getElementById("E_textbox").value;
-  console.log(new_loc);
-  console.log(new_address);
-  var updated_data = {
-    srno: srno,
-    ID: ID,
-    LocationName: new_loc,
-    address: new_address,
-  };
-
-  cur_data.push(updated_data);
-  localStorage.removeItem("TABLE");
-  localStorage.setItem("TABLE", JSON.stringify(cur_data));
-};
-
-// this will insiate popup of delete  and then connect it with  delete function
-const open_delete_popup = (srno) => {
-  console.log("hello1");
-  const data = document.getElementById("Delete_view");
-  data.classList.remove("disblank");
-
-  const call_fun = document.getElementById("button-popup");
-  console.log(data);
-  call_fun.innerHTML = `<button onclick="Delete(${srno})" type="button" class="yes"> YES </button> 
-                        <button onclick="close_box_del()" type="submit" class="no">NO</button>`;
-};
-
-// to delete data
-const Delete = (srno) => {
-  console.log("inside-delete");
-  let table;
-
-  table = localStorage.getItem("TABLE");
-  console.log(table);
-  table = JSON.parse(table);
-  console.log(table);
-
-  if (table.length <= 1) {
-    localStorage.removeItem("TABLE");
-  } else {
-    let cur = table.filter((data) => data.srno != srno);
-    console.log(cur);
-
-    localStorage.removeItem("TABLE");
-    localStorage.setItem("TABLE", JSON.stringify(cur));
+    if (table.length == 0) {
+      localStorage.removeItem("TABLE");
+    } else {
+      let cur = table.filter((data) => data.ID != ID);
+      localStorage.removeItem("TABLE");
+      localStorage.setItem("TABLE", JSON.stringify(cur));
+    }
+  } catch (err) {
+    console.log(err);
   }
   close_box_del();
   tableRefresh();
 };
 
-// to view data
+// For view section
 
-const view = (srno) => {
-  const ID = document.getElementById("ID_veiw_data");
-  const name_veiw_data = document.getElementById("name_veiw_data");
-  const location_drcr = document.getElementById("location_drcr_veiw_data");
-  const viewbox = document.getElementById("view_data");
-  viewbox.classList.remove("disblank");
-
-  let table = localStorage.getItem("TABLE");
-  table = JSON.parse(table);
-
-  let cur = table.filter((data) => {
-    return data.srno == srno;
-  });
-
-  ID.value = cur[0].ID;
-  name_veiw_data.value = cur[0].LocationName;
-  location_drcr.value = cur[0].address;
+const close_box = () => {
+  const data = document.getElementById("view_data");
+  data.classList.add("disblank");
 };
 
-// script for data opreation
+const view = (ID) => {
+  const idViewData = document.getElementById("ID_veiw_data");
+  const name_veiw_data = document.getElementById("name_veiw_data");
+  const location_descr = document.getElementById("location_descr_veiw_data");
+  const viewbox = document.getElementById("view_data");
+  viewbox.classList.remove("disblank");
+  let table = localStorage.getItem("TABLE");
+  table = JSON.parse(table);
+  let cur = table.filter((data) => {
+    return data.ID == ID;
+  });
+
+  idViewData.value = cur[0].ID;
+  name_veiw_data.value = cur[0].LocationName;
+  location_descr.value = cur[0].address;
+};
+
+// script for data operation
 const addData = () => {
   let Location_name = document.querySelector("#Location_name").value;
   let Address = document.querySelector("#textbox").value;
   let ID = document.querySelector("#ID").value;
 
-  console.log(Address);
-  console.log(ID);
-
-  console.log(Location_name);
-
   if (Location_name && Address) {
-    // data opreation
+    // data oprertion
     let erro = document.getElementById("total-error");
     erro.innerText = "";
 
     if (localStorage) {
-      console.log("data");
-
       if (localStorage.getItem("TABLE")) {
-        let lo = localStorage.getItem("TABLE");
+        let tableData = localStorage.getItem("TABLE");
 
-        lo = JSON.parse(lo);
+        tableData = JSON.parse(tableData);
         var data = {
-          srno: lo.length + 1,
+          srno: tableData.length + 1,
           ID: ID,
           LocationName: Location_name,
           address: Address,
         };
 
-        lo.push(data);
+        tableData.push(data);
         localStorage.removeItem("TABLE");
-        localStorage.setItem("TABLE", JSON.stringify(lo));
+        localStorage.setItem("TABLE", JSON.stringify(tableData));
         tableRefresh();
         alert("Data added successfully");
         addRecordPop();
@@ -184,7 +195,6 @@ const addData = () => {
         localStorage.setItem("TABLE", JSON.stringify([{ ...data }]));
         tableRefresh();
         location.reload(true);
-
         addRecordPop();
       }
     }
